38876ef2989872e57f3f9a1143d81d6922d80df7
urganciemrah3434-byte <urganciemrah3434@gmail.com>
2026-07-21 09:56:59 +0300

    Create firebase-config.js

---
+++ css/js/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDiEf4SXghJpXUiHIrflLk8lnvaAk7LnUI",
+  authDomain: "takasla-e2013.firebaseapp.com",
+  projectId: "takasla-e2013",
+  storageBucket: "takasla-e2013.firebasestorage.app",
+  messagingSenderId: "324778736339",
+  appId: "1:324778736339:web:d1c25d9aff143c35833226",
+  measurementId: "G-CX29N06BZ7"
+};
+
+const app = initializeApp(firebaseConfig);
+
+console.log("TAKASLA Firebase bağlantısı başarılı!");
